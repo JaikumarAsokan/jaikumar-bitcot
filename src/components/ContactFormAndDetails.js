be8695b72f50b95262ecc26830244e774c8fbf06
JaikumarAsokan <jaikumarasokan96@gmail.com>
2024-09-14 21:16:30 +0530

    added number validation

--- src/components/ContactFormAndDetails.js
+++ src/components/ContactFormAndDetails.js
@@ -1,13 +1,17 @@
-import React, { useState, useEffect } from 'react'; 
+import React, { useState, useEffect } from "react";
 
 const ContactFormAndDetails = ({ contact, onSave, onClose, mode }) => {
-  const [formContact, setFormContact] = useState({ name: '', mobile: '', email: '' });
+  const [formContact, setFormContact] = useState({
+    name: "",
+    mobile: "",
+    email: "",
+  });
 
   useEffect(() => {
     if (contact) {
       setFormContact(contact);
     } else {
-      setFormContact({ name: '', mobile: '', email: '' });
+      setFormContact({ name: "", mobile: "", email: "" });
     }
   }, [contact]);
 
@@ -18,7 +22,7 @@ const ContactFormAndDetails = ({ contact, onSave, onClose, mode }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!formContact.name || !formContact.mobile || !formContact.email) {
-      alert('All fields are required');
+      alert("All fields are required");
       return;
     }
     onSave(formContact);
@@ -27,25 +31,56 @@ const ContactFormAndDetails = ({ contact, onSave, onClose, mode }) => {
   return (
     <div className="modal">
       <div className="modal-content">
-        <span className="close" onClick={onClose}>&times;</span>
-          <>
-            <h2>{mode === 'add' ? 'Add Contact' : mode === 'edit' ? 'Edit Contact' : 'View Contact'}</h2>
-            <form onSubmit={handleSubmit}>
-             <fieldset disabled={mode === 'view'} className='fieldset'>
-              <label><strong>Name</strong> 
-                <input type="text" name="name" value={formContact.name} onChange={handleChange} />
+        <span className="close" onClick={onClose}>
+          &times;
+        </span>
+        <>
+          <h2>
+            {mode === "add"
+              ? "Add Contact"
+              : mode === "edit"
+              ? "Edit Contact"
+              : "View Contact"}
+          </h2>
+          <form onSubmit={handleSubmit}>
+            <fieldset disabled={mode === "view"} className="fieldset">
+              <label>
+                <strong>Name</strong>
+                <input
+                  type="text"
+                  name="name"
+                  value={formContact.name}
+                  onChange={handleChange}
+                />
               </label>
-              <label><strong>Mobile</strong> 
-              <input type='text' pattern="[0-9]*" inputmode="numeric" name="mobile" value={formContact.mobile} onChange={handleChange} />
+              <label>
+                <strong>Mobile</strong>
+                <input
+                  type="text"
+                  pattern="[0-9]*"
+                  inputmode="numeric"
+                  name="mobile"
+                  value={formContact.mobile}
+                  onChange={handleChange}
+                />
               </label>
-              <label><strong>Email</strong> 
-                <input type="email" name="email" value={formContact.email} onChange={handleChange} />
+              <label>
+                <strong>Email</strong>
+                <input
+                  type="email"
+                  name="email"
+                  value={formContact.email}
+                  onChange={handleChange}
+                />
               </label>
-              { mode !== 'view'&&  <button type="submit">{mode === 'add' ? 'Add Contact' : 'Update Contact'}</button>}
-              </fieldset>
-            </form>
-          </>
-        
+              {mode !== "view" && (
+                <button type="submit">
+                  {mode === "add" ? "Add Contact" : "Update Contact"}
+                </button>
+              )}
+            </fieldset>
+          </form>
+        </>
       </div>
     </div>
   );
